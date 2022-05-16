d400541abc54b7594873de700577092c67c45d22
marlondellys <89539489+marlondellys@users.noreply.github.com>
2022-05-16 09:52:45 -0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,8 +1,8 @@
 class User(object):
 
-    SRV_IP = '192.168.0.8'
-    SRV_PORT = '1521'
-    DB_SERVICE = 'PRD'
+    SRV_IP = '' # Server IP on database.
+    SRV_PORT = '' # Port Server.
+    DB_SERVICE = '' # Service Name on Server.
     DSN = SRV_IP + ':' + SRV_PORT + '/' + DB_SERVICE + ':' + SRV_PORT + '/' + DB_SERVICE 
     DB_USER = '' # You user on oracle database.
     DB_PASSWORD = '' # You password on oracle database.
